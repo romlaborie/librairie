51d23066b21dc46463f61a5433ca0bb039043980
Romain <romain.est.laborie@gmail.com>
2022-11-18 09:48:49 +0100

    Initial commit

---
+++ src/main/java/com/epsiBibliProjet/librairie/LibrairieApplication.java
@@ -0,0 +1,13 @@
+package com.epsiBibliProjet.librairie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrairieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrairieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsiBibliProjet/librairie/LibrairieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsiBibliProjet.librairie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrairieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
